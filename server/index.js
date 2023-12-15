898945eae15443619c5e9b7713db236db4f3207e
tu83bnh9m@code.edu.az <tu83R2GJ2@code.edu.az>
2023-12-15 13:46:33 +0400

    added new part

--- server/index.js
+++ server/index.js
@@ -1,106 +1,100 @@
-const express=require("express")
-const cors=require("cors")
-const bodyParser=require("body-parser")
-const mongoose=require("mongoose")
-const dotenv=require("dotenv")
-
-dotenv.config()
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose, { model, connect } from "mongoose";
+import Users from "./Models/usersModel.js"
+import userRouter from "./Routes/usersRoute.js"
 
 
- const {Schema}=mongoose;
+// const express=require("express")
+// const cors=require("cors")
+// const bodyParser=require("body-parser")
+// const mongoose=require("mongoose")
 
-const userSchema=new Schema({
-    name:{type:String,required:true},
-    surname:{type:String,required:true},
-    age:{type:Number,required:true},
-    image:{type:String,required:true}
-},{timestamps:true}
-)
+
+
+
 const app=express()
 
 
 //Midleware
 app.use(cors())
 app.use(bodyParser.json())
+app.use("/users",userRouter)
 
-const Users=mongoose.model("users",userSchema)
+// //Get All Users
 
-//Get All Users
+// app.get("/users",async (req,res)=>{
+//     try {
+//         const users=await Users.find({})
+//         res.send(users)
+//     } catch (error) {
+//         res.status(500).json({message:error})
+//     }
+// })
 
-app.get("/users",async (req,res)=>{
-    try {
-        const users=await Users.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).json({message:error})
-    }
-})
+// //User get by id
 
-//User get by id
-
-app.get("/users/:id",async (req,res)=>{
-    try {
-        const user=await Users.findById(req.params.id)
+// app.get("/users/:id",async (req,res)=>{
+//     try {
+//         const user=await Users.findById(req.params.id)
         
-            res.send(user)
+//             res.send(user)
        
-    } catch (error) {
-        res.status(500).json({message:error})
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message:error})
+//     }
+// })
 
 
-//Add User
-app.post("/users",(req,res)=>{
-    const user=new Users({
-        name:req.body.name,
-        surname:req.body.surname,
-        age:req.body.age,
-        image:req.body.image
-    })
-    user.save()
-    res.send({message:"User Created"})
-})
+// //Add User
+// app.post("/users",(req,res)=>{
+//     const user=new Users({
+//         name:req.body.name,
+//         surname:req.body.surname,
+//         age:req.body.age,
+//         image:req.body.image
+//     })
+//     user.save()
+//     res.send({message:"User Created"})
+// })
 
-//User Update
-app.put("/users/:id",async (req,res)=>{
-    try {
-        const user=await Users.findByIdAndUpdate(req.params.id)
+// //User Update
+// app.put("/users/:id",async (req,res)=>{
+//     try {
+//         const user=await Users.findByIdAndUpdate(req.params.id)
 
-        if(user){
-            user.name=req.body.name,
-            user.surname=req.body.surname,
-            user.age=req.body.age,
-            user.image=req.body.image
+//         if(user){
+//             user.name=req.body.name,
+//             user.surname=req.body.surname,
+//             user.age=req.body.age,
+//             user.image=req.body.image
 
-            await user.save()
-            res.json(user)
-        }else{
-            res.status(404).json({message:"Not Found"})
-        }
-    } catch (error) {
-        res.status(500).json({message:error})
-    }
-})
+//             await user.save()
+//             res.json(user)
+//         }else{
+//             res.status(404).json({message:"Not Found"})
+//         }
+//     } catch (error) {
+//         res.status(500).json({message:error})
+//     }
+// })
 
-//Delete User
+// //Delete User
 
-app.delete("/users/:id",async (req,res)=>{
-    try {
-       await Users.findByIdAndDelete(req.params.id)
-        res.status(200).json({message:"User Deleted"})
+// app.delete("/users/:id",async (req,res)=>{
+//     try {
+//        await Users.findByIdAndDelete(req.params.id)
+//         res.status(200).json({message:"User Deleted"})
         
-    } catch (error) {
-        res.status(500).json({message:error})
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({message:error})
+//     }
+// })
 
-const PORT=process.env.PORT
-const url=process.env.CONNECTION_URL.replace("<password>", process.env.PASSWORD)
 
-mongoose.connect(url).catch(err=>console.log("Db not connect" + err))
+connect('mongodb+srv://asli:asli123@cluster0.euc1dwy.mongodb.net/').catch(err=>console.log("Db not connect" + err))
 
-app.listen(PORT,()=>{
+app.listen(5000,()=>{
         console.log("Server Connection");
 })
